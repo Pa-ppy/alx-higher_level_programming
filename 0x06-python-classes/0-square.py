282de4bfc2321975a54ec6517fe45d36b5179247
Pa-ppy <rayjacks07@gmail.com>
2024-10-01 18:15:35 -0700

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square
+"""
+
+class Square:
+    """
+    This class represents a square but does not define any attributes or methods.
+    """
+    pass
