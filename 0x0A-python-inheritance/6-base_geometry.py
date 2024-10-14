d97a1235479449f34b28d0f10865231d86806a13
Pa-ppy <rayjacks07@gmail.com>
2024-10-14 16:29:45 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    Class BaseGeometry with an area method
+    """
+
+    def area(self):
+        """
+        Raises an exception with message 'area is not implemented'
+        """
+        raise Exception("area() is not implemented")
