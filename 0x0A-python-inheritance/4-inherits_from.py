b1f2e4ed1a9a029dc7362259523998dce167fc83
Pa-ppy <rayjacks07@gmail.com>
2024-10-14 16:23:13 -0700

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited from the specified class
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
