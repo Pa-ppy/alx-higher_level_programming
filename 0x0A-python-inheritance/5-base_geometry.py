af0148ff27dda18d5ba7e2bb3ea48d48131c8ae8
Pa-ppy <rayjacks07@gmail.com>
2024-10-16 16:05:33 -0700

    Task 5

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class that will be used as a base for future
+geometry-related classes.
+"""
+
+
 class BaseGeometry:
     """
-    Empty class BaseGeometry
+    Empty class BaseGeometry used as a base for geometry-related classes.
     """
     pass
