97fe22d64558cbde491b0b5642c43282a29a8c7b
Pa-ppy <rayjacks07@gmail.com>
2024-10-16 15:42:29 -0700

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a class that contains a method that prints
+the list sorted in ascending order
+"""
+
+
 class MyList(list):
     """
     Inherits list and provides a method to print sorted list
