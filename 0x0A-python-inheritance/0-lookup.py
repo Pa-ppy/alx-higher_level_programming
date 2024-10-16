977a8ef8cba2db73b045aac7e20f2da51de1a15c
Pa-ppy <rayjacks07@gmail.com>
2024-10-16 15:39:34 -0700

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 This module defines a function lookup that returns the list
 of available attributes
 """
+
 def lookup(obj):
     """
     Returns the list of the available attributes/methods of an object
