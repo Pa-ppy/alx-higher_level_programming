c5a88dfa1c1bc5ab0d91462192c36f67eaf9e6bb
Pa-ppy <rayjacks07@gmail.com>
2024-11-20 20:12:39 +0000

    Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- List all cities in California
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+	SELECT states.id
+	FROM states
+	WHERE states.name = 'California'
+)
+ORDER BY cities.id ASC;
