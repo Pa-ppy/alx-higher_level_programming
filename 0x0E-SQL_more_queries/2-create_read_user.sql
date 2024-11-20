56e02c59a149620209e319b3bc49630460742f56
Pa-ppy <rayjacks07@gmail.com>
2024-11-20 15:37:26 +0000

    Task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create the database hbtn_0d_2 and user_0d_2 with SELECT privilege
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'User0d2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
