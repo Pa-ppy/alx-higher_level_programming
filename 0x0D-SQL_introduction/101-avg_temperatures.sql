634624dce9da9427be4c55fdaf803bf6ec26e117
Pa-ppy <rayjacks07@gmail.com>
2024-11-20 05:56:05 +0000

    Task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- Calculates the average temperatures for each city and order by temperature descending
-SELECT city,
-	ROUND(AVG(temperature), 4) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
