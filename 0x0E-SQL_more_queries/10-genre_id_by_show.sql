6728fc313a69cef6721828aa157d09f43856aeeb
Pa-ppy <rayjacks07@gmail.com>
2024-11-21 09:31:09 +0000

    Task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+USE hbtn_0d_tvshows;
+-- List shows with at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
