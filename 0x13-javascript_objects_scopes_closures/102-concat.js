ab7385213216cd6b3295ff3049753cffe9ac5abd
Pa-ppy <rayjacks07@gmail.com>
2024-12-20 17:40:23 +0000

    JS - O, S, and C

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const destination = process.argv[4];
+
+const contentA = fs.readFileSync(fileA, 'utf-8');
+const contentB = fs.readFileSync(fileB, 'utf-8');
+fs.writeFileSync(destination, contentA + contentB);
