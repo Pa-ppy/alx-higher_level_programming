a1e22f6b8703cbbcb4f6a01a5a79f07f56e78dd4
Pa-ppy <rayjacks07@gmail.com>
2024-12-29 00:44:48 +0000

    O-R mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+     def list_cities():
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute(
+            """
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC;
+            """
+        )
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+
+    if sys.argv[0].endswith("0-select_states.py"):
+        list_states()
+    elif sys.argv[0].endswith("1-filter_states.py"):
+        filter_states()
+    elif sys.argv[0].endswith("2-my_filter_states.py"):
+        my_filter_states()
+    elif sys.argv[0].endswith("3-my_safe_filter_states.py"):
+        my_safe_filter_states()
+    elif sys.argv[0].endswith("4-cities_by_state.py"):
+        list_cities()
