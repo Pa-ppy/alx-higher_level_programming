4cb433a140adbede50148be7ad3c5f7b95211fd1
Pa-ppy <rayjacks07@gmail.com>
2024-12-30 07:44:38 +0000

    O-R mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == "__main__":
     def list_states():
+        """
+        Connects to the database and retrieves all states
+        ordered by their ID in ascending order.
+        """
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
