5e4b6a01cfdda8473232ef4b447416d21f56868e
Pa-ppy <rayjacks07@gmail.com>
2025-01-01 04:12:41 +0000

    O-R mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
+    def list_states():
         """
         Connects to the database and retrieves all states
         ordered by their ID in ascending order.
         """
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC;")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
+        try:
+            db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3])
+            cur = db.cursor()
+            cur.execute("SELECT * FROM states ORDER BY id ASC;")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+        except MySQLdb.Error as e:
+            print(f"Error: {e}")
+        finally:
+            cur.close()
+            db.close()
