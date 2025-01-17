bd9e2d59289150a3e1c6d7b90627fd96bb063ef3
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:20:10 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module sends a POST request to http://0.0.0.0:5000/search_user."""
+import requests
+import sys
+
+
+def search_user(letter):
+    """Send POST request with letter and handle JSON response."""
+    if not letter:
+        letter = ""
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+    
+    try:
+        data = response.json()
+        if data:
+            print(f"[{data['id']}] {data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    search_user(sys.argv[1] if len(sys.argv) > 1 else "")
