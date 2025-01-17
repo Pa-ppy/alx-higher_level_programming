4afde5c209288c106cb43ff111a7505812a82f27
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:14:49 +0000

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module handles HTTP errors and displays error code."""
+import urllib.request
+import urllib.error
+import sys
+
+
+def fetch_url(url):
+    """Fetch URL and display body or error code."""
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    fetch_url(sys.argv[1])
