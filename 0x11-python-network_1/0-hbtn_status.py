2847a65c527a0c449cda3a4d676c3cb2ca3c0f43
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 17:26:46 +0000

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,12 +7,12 @@ import urllib.request
 
 def fetch_status():
     """Fetch and display the body response."""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as
-    response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
         body = response.read()
         print(f"\t- type: {type(body)}")
         print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decide('utf-8')}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
 
 
 if __name__ == "__main__":
