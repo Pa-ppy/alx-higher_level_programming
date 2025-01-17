8d5db132b628faeb1301e5bd95474cabd610528f
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:18:23 +0000

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module takes in a URL and an email address."""
+import requests
+import sys
+
+
+def send_email(url, email):
+    """Send POST request with email and display the body of the response."""
+    response = requests.post(url, data={'email': email})
+    print(f"Your email is: {response.text}")
+
+if __name__ == "__main__":
+    send_email(sys.argv[1], sys.argv[2])
