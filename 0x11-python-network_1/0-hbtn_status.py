cf9b34ca4408c835fc8a7f4ab8907944f7c6634a
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:13:48 +0000

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def fetch_status():
+    """Fetch and display the body response."""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decide('utf-8')}")
+
+if __name__ == "__main__":
+    fetch_status()
