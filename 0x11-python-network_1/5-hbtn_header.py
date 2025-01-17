e5107dd67da692ac17a88bd8e88f6c589c38ebb1
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:17:59 +0000

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module takes in a URL and sends a request to the URL."""
+import requests
+import sys
+
+
+def fetch_request_id(url):
+    """Fetch and display the value of X-Request-Id header."""
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    fetch_request_id(sys.argv[1])
