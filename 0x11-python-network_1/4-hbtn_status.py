3a6ef704eda16a1a00215ce8e8a66df99ebaffdd
Pa-ppy <rayjacks07@gmail.com>
2025-01-17 16:15:15 +0000

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module fetches URL with requests."""
+import requests
+
+
+def fetch_status():
+    """Fetch and display the body response."""
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
+    print(f"\t- utf8 content: {response.text}")
+
+if __name__ == "__main__":
+    fetch_status()
