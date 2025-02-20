761a5f0afee3b7305c9fafc490d1fad61fe34a88
Pa-ppy <rayjacks07@gmail.com>
2025-02-20 00:11:06 +0000

    Task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,4 @@
+/* global $ */
 $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   data.results.forEach(movie => {
     $('#list_movies').append('<li>' + movie.title + '</li>');
